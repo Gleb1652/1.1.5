80e085e916a41018f706aedb10a34ee8d60f713d
ogorodtrue <semenenkogleb195@gmail.com>
2023-03-20 14:51:47 +0300

    end

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,22 +8,18 @@ import java.sql.SQLException;
 import static jm.task.core.jdbc.dao.UserDaoJDBCImpl.connection;
 
 public class Main {
-    private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+
         userService.createUsersTable();
 
-        userService.saveUser("aboba", "testLastName1", (byte) 18);
-        userService.saveUser("aboba", "testLastName2", (byte) 18);
-        userService.saveUser("aboba", "testLastName3", (byte) 18);
-        userService.saveUser("aboba", "testLastName4", (byte) 18);
+        userService.saveUser("Aboba1", "lastName1", (byte) 18);
+        userService.saveUser("Aboba2", "lastName2", (byte) 18);
+        userService.saveUser("Aboba3", "lastName3", (byte) 18);
+        userService.saveUser("Aboba4", "lastName4", (byte) 18);
 
+        userService.removeUserById(1);
         userService.getAllUsers();
-        userService.cleanUsersTable();
         userService.dropUsersTable();
-        try {
-            connection.close();
-        } catch (SQLException e) {
-
-        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/testDB";
+    private static final String URL = "jdbc:mysql://localhost:3306";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "v702275";
 
     private static Connection connection;
 
